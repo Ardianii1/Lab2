22927d06e48eead5202ffb2e69c881d0d85e9a06
Ardianii1 <ah54088@ubt-uni.net>
2023-07-31 02:16:41 +0200

    added a loader

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,38 +1,36 @@
 // app/layout.tsx
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
-import { ThemeProvider } from '../providers/theme-provider'
-import { ModalProvider } from '@/providers/modal-provider'
-import { ToastProvider } from '@/providers/toast-provider'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "../providers/theme-provider";
+import { ModalProvider } from "@/providers/modal-provider";
+import { ToastProvider } from "@/providers/toast-provider";
+import NextJsTopLoader from "nextjs-toploader";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Admin Dashboard',
-  description: 'Admin dashboard for e-comerce stores',
-}
+  title: "Admin Dashboard",
+  description: "Admin dashboard for e-comerce stores",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-        <ThemeProvider 
-            attribute="class" 
-            defaultTheme="system" 
-            enableSystem
-            >
-              <ToastProvider/>
-              <ModalProvider/>
-          {children}
-            </ThemeProvider>
-          </body>
+          <NextJsTopLoader />
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <ToastProvider />
+            <ModalProvider />
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
